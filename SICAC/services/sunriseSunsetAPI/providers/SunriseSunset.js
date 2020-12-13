ab76bb51f3fac2cd00b8af48582b9d70ce7e52b4
Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>
2020-12-13 16:09:06 +0100

    Refactoring SunriseSunsetAPI in services

---
+++ SICAC/services/sunriseSunsetAPI/providers/SunriseSunset.js
@@ -0,0 +1,27 @@
+
+const getInformationFromSunriseSunsetAPI = async () => {
+  // For now the location is Bordeaux
+  // TODO Modify latitude and longitude with location from phone (when location is done in the project)
+    try {
+      let response = await fetch(
+        'https://api.sunrise-sunset.org/json?lat=44.8333&lng=-0.5667&date=today'
+      );
+      let json = await response.json();
+      return json.results;
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+export const getTodayUTCSunrise = async () => {
+  let json = await getInformationFromSunriseSunsetAPI();
+  console.log("Sunrise : " + json.sunrise);
+  return json.sunrise;
+};
+
+export const getTodayUTCSunset = async () => {
+  let json = await getInformationFromSunriseSunsetAPI();
+  console.log("Sunset : " + json.sunset);
+  return json.sunset;
+};
+
