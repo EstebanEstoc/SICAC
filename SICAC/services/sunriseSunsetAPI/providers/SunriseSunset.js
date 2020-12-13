0ffa3f323fead6152d2528d71b586b33a609d050
Juliette Deguillaume <Juliette.Deguillaume@bordeaux-inp.fr>
2020-12-13 16:16:30 +0100

    Format code + adding comments

--- SICAC/services/sunriseSunsetAPI/providers/SunriseSunset.js
+++ SICAC/services/sunriseSunsetAPI/providers/SunriseSunset.js
@@ -2,26 +2,26 @@
 const getInformationFromSunriseSunsetAPI = async () => {
   // For now the location is Bordeaux
   // TODO Modify latitude and longitude with location from phone (when location is done in the project)
-    try {
-      let response = await fetch(
-        'https://api.sunrise-sunset.org/json?lat=44.8333&lng=-0.5667&date=today'
-      );
-      let json = await response.json();
-      return json.results;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    let response = await fetch(
+      'https://api.sunrise-sunset.org/json?lat=44.8333&lng=-0.5667&date=today'
+    );
+    let json = await response.json();
+    return json.results;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getTodayUTCSunrise = async () => {
   let json = await getInformationFromSunriseSunsetAPI();
-  console.log("Sunrise : " + json.sunrise);
+  console.log("Sunrise : " + json.sunrise); // Only to debug during demo
   return json.sunrise;
 };
 
 export const getTodayUTCSunset = async () => {
   let json = await getInformationFromSunriseSunsetAPI();
-  console.log("Sunset : " + json.sunset);
+  console.log("Sunset : " + json.sunset); // Only to debug during demo
   return json.sunset;
 };
 
