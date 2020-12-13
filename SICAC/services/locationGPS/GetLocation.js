848567df321062cf0f439afa128576332a4fb978
Lucaazel <luca.zelmans@gmail.com>
2020-12-13 19:52:28 +0100

    sprint luca

---
+++ SICAC/services/locationGPS/GetLocation.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
+export default class GetLocation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 },
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+      </View>
+    );
+  }
+}
