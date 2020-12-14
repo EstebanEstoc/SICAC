7fe57ba7e8ac771cb7b296c61265d67a02b954ed
brobin002 <balth.robin@gmail.com>
2020-12-14 19:40:12 +0100

    ConditionsType avec le dossier data (crée à la racine)

---
+++ SICAC/data/Conditions.js
@@ -0,0 +1,37 @@
+export const conditions = [
+    {
+        id: 1,
+        Title: "Home",
+        IconName: "home"
+    },
+    {
+        id: 2,
+        Title: "Location",
+        IconName: "place"
+    },
+    {
+        id: 3,
+        Title: "Calendar",
+        IconName: "today"
+    },
+    {
+        id: 4,
+        Title: "Duration",
+        IconName: "schedule"
+    },
+    {
+        id: 5,
+        Title: "Bluetooth",
+        IconName: "bluetooth"
+    },
+    {
+        id: 6,
+        Title: "Time",
+        IconName: "flare"
+    },
+    {
+        id: 7,
+        Title: "Alert",
+        IconName: "notifications"
+    }
+]
