9ce7c217b9be866baf373925e09653a19ef69392
EstebanEstoc <esteban.estoc@enseirb-matmeca.fr>
2020-12-14 20:53:15 +0100

    Done for createScenario

--- SICAC/components/NewScenario/Actions/Actions.jsx
+++ SICAC/components/NewScenario/Actions/Actions.jsx
@@ -1,85 +1,69 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
-import { Card, Icon } from 'react-native-elements'
-import { Transition, Transitioning } from 'react-native-reanimated';
-import scenarios from 'helpers/ActionsData';
-import styles, { mainColor, secondaryColor } from './ActionsStyles'
+import React from "react";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { Transition, Transitioning } from "react-native-reanimated";
+import styles, { mainColor, secondaryColor } from "./ActionsStyles";
 
 const transition = (
   <Transition.Together>
-    <Transition.In type='scale' durationMs={200} />
+    <Transition.In type="scale" durationMs={200} />
     <Transition.Change />
-    <Transition.Out type='scale' durationMs={200} />
+    <Transition.Out type="scale" durationMs={200} />
   </Transition.Together>
 );
 
-const Title = ({text}) => (
-  <Text style={styles.title}>
-    {text}
-  </Text>
-)
+const Title = ({ text }) => <Text style={styles.title}>{text}</Text>;
 
 export default function Actions() {
-  const [currentScenarios, setCurrentScenarios] = React.useState(scenarios)
-  const ref = React.useRef()
+  const scenario = useSelector((state) => state.createScenario);
+  const ref = React.useRef();
 
   return (
     <ScrollView containerStyle={styles.container}>
       <Transitioning.View
-                ref={ref}
-                transition={transition}
-                style={styles.container}
+        ref={ref}
+        transition={transition}
+        style={styles.container}
       >
         <Title text="Select your action(s)" />
         <View style={styles.itemList}>
-          {
-            currentScenarios.map((scenario, index) => {
+          {scenario.then &&
+            scenario.then.map((scenario, index) => {
               return (
-                <Card 
-                  key={index}
-                  containerStyle={styles.card}
-                >
-                  <Text 
-                    style={styles.cardText}
-                  >
-                    {scenario.name}
-                  </Text>
+                <Card key={index} containerStyle={styles.card}>
+                  <Text style={styles.cardText}>{scenario.name}</Text>
                   <Icon
-                    containerStyle={{ alignSelf: 'flex-end' }}
-                    name='trash'
-                    type='font-awesome'
+                    containerStyle={{ alignSelf: "flex-end" }}
+                    name="trash"
+                    type="font-awesome"
                     onPress={() => {
-                      ref.current.animateNextTransition()
-                      setCurrentScenarios(previousScenarios => previousScenarios.filter((item, id) => index !== id))
+                      ref.current.animateNextTransition();
+                      setCurrentScenarios((previousScenarios) =>
+                        previousScenarios.filter((item, id) => index !== id)
+                      );
                     }}
                   />
                 </Card>
               );
-            })
-          }
+            })}
         </View>
         <View style={styles.addButtonSection}>
-          <Text>
-            |
-          </Text>
-          <Text>
-            |
-          </Text>
+          <Text>|</Text>
+          <Text>|</Text>
           <TouchableOpacity
             activeOpacity={0.8}
-            onPress={() => console.log('→ Redirect to new scenario page')} 
+            onPress={() => console.log("→ Redirect to new scenario page")}
           >
             <Icon
               containerStyle={styles.addButtonIcon}
               reverse
               size={35}
-              name='plus'
-              type='font-awesome'
+              name="plus"
+              type="font-awesome"
               color={mainColor}
             />
-            <Text style={styles.addButtonText}>
-              Add an action
-            </Text>
+            <Text style={styles.addButtonText}>Add an action</Text>
           </TouchableOpacity>
         </View>
       </Transitioning.View>
--- SICAC/reducers/rootReducer.js
+++ SICAC/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import authentication from './authentication/authenticationSlice'
 import user from './authentication/userSlice'
+import scenarios from './scenarios/scenariosSlice'
+import createScenario from './scenarios/createScenarioSlice'
 
 export default combineReducers({
   authentication,
-  user
+  user,
+  scenarios,
+  createScenario
 })
