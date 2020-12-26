793dd2da6b78f64760f923ff5f66fba3b9d1e18d
brobin002 <balth.robin@gmail.com>
2020-12-26 13:25:02 +0100

    icon update (actions et conditions)

--- SICAC/data/Conditions.js
+++ SICAC/data/Conditions.js
@@ -13,45 +13,57 @@ export const conditions = [
     },
     {
         id: 3,
-        Title: "Calendar",
-        IconName: "today",
+        Title: "Have to answer form",
+        IconName: "description",
         Condition: ""
     },
     {
         id: 4,
-        Title: "Duration",
+        Title: "Hour",
         IconName: "schedule",
         Condition: ""
     },
     {
         id: 5,
-        Title: "Time",
+        Title: "Time of day",
         IconName: "flare",
         Condition: "TimeOfDayCondition"
     },
     {
         id: 6,
-        Title: "Alert",
-        IconName: "notifications",
+        Title: "Have to walk",
+        IconName: "nature",
         Condition: ""
     },
     {
         id: 7,
-        Title: "Earphones (Bluetooth)",
+        Title: "Connexion to earphones",
         IconName: "headset",
         Condition: ""
     },
     {
         id: 8,
-        Title: "Speaker (Bluetooth)",
+        Title: "Connexion to speaker",
         IconName: "speaker",
         Condition: ""
     },
     {
         id: 9,
-        Title: "TV (Bluetooth)",
-        IconName: "tv",
-        Condition: "Bluetooth"
+        Title: "Have an appointment",
+        IconName: "today",
+        Condition: ""
+    },
+    {
+        id: 10,
+        Title: "Have to take pills",
+        IconName: "healing",
+        Condition: ""
+    },
+    {
+        id: 11,
+        Title: "High\n heart rate",
+        IconName: "favorite",
+        Condition: ""
     }
     
 ]
